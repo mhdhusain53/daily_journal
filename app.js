9a003038bbb0d309ea3238b34271d4685ed991ac
Mohammed Husain <mhdhusain53@gmail.com>
2022-03-20 12:43:41 +0530

    add

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var lodash=require('lodash');
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,13 +16,19 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/dailyJournal");
 
-let posts=[];
+const postsSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", itemsSchema);
 
 
 app.get("/",function(req,res){
 
-  res.render("home",{loremIpsum1:homeStartingContent, allPosts:posts});
+  res.render("home",{loremIpsum1:homeStartingContent});
 });
 app.get("/about",function(req,res){
   res.render("about",{loremIpsum2:aboutContent});
@@ -33,11 +40,11 @@ app.get("/compose",function(req,res){
   res.render("compose",{loremIpsum3:contactContent});
 });
 app.post("/compose",function(req,res){
-  const post={
+  const post = new Post({
     title: req.body.inputTitle,
     content: req.body.inputPost
-  };
-  posts.push(post);
+});
+
   res.redirect("/");
 });
 
@@ -49,19 +56,14 @@ app.get("/delete",function(req,res){
 });
 
 app.get("/posts/:part",function(req,res){
-  posts.forEach(function(elent){
+  name=req.params.part;
+  posts.forEach(function(element){
     if(lodash.lowerCase(elent.title)===lodash.lowerCase(req.params.part)){
-      res.render("post",{post:elent});
+      res.render("post",{post:element});
     }
   });
 
 });
-
-
-
-
-
-
 
 
 
